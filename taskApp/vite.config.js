9ab1a9bd6527cbf86b0cdd43c4d258b100e07b0c
Leo Marquez <142362735+LJMarquez@users.noreply.github.com>
2025-02-20 23:00:51 +0000

    pused 2/20

--- taskApp/vite.config.js
+++ taskApp/vite.config.js
@@ -14,3 +14,21 @@ export default defineConfig({
         }),
     ],
 });
+
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//     ],
+//     server: {
+//         hmr: {
+//             host: `${process.env.CODESPACE_NAME}-5173.app.github.dev`,
+//         },
+//         host: '0.0.0.0',
+//     },
+// });
